e9b6b509a9056800dec1c5c16298d63ed054cc26
Peter Jang <peter.x.jang@gmail.com>
2024-07-31 13:47:32 -0500

    Add products index action

---
+++ src/ProductsIndex.jsx
@@ -0,0 +1,14 @@
+export function ProductsIndex({ products }) {
+  return (
+    <div>
+      <h1>All products</h1>
+      {products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>Price: {product.price}</p>
+          <p>{product.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
