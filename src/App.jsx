51f9bec5bbf537312c819a9537daed9564a491e1
Peter Jang <peter.x.jang@gmail.com>
2024-07-31 13:39:48 -0500

    Add authentication components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import { Header } from "./Header";
 import { ProductsPage } from "./ProductsPage";
 import { Footer } from "./Footer";
+import { SignupPage } from "./SignupPage";
+import { LoginPage } from "./LoginPage";
 
 function App() {
   return (
     <div>
       <Header />
+      <SignupPage />
+      <LoginPage />
       <ProductsPage />
       <Footer />
     </div>
