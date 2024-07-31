e6f7fe103ce70fba752e7078c87f4007df197c7c
Peter Jang <peter.x.jang@gmail.com>
2024-07-31 14:02:49 -0500

    Add products new/create action

--- src/ProductsPage.jsx
+++ src/ProductsPage.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { ProductsIndex } from "./ProductsIndex";
+import { ProductsNew } from "./ProductsNew";
 
 export function ProductsPage() {
   const [products, setProducts] = useState([]);
@@ -11,10 +12,25 @@ export function ProductsPage() {
     });
   };
 
+  const handleCreate = (params, successCallback, failureCallback) => {
+    axios
+      .post("http://localhost:3000/products.json", params)
+      .then((response) => {
+        setProducts([...products, response.data]);
+        successCallback();
+      })
+      .catch((error) => {
+        if (error.response.data.errors) {
+          failureCallback(error.response.data.errors);
+        }
+      });
+  };
+
   useEffect(handleIndex, []);
 
   return (
     <main>
+      <ProductsNew onCreate={handleCreate} />
       <ProductsIndex products={products} />
     </main>
   );
