beece0985886355bafd126b3b5003eaab06ebbee
Peter Jang <peter.x.jang@gmail.com>
2024-08-01 11:49:56 -0500

    Use <Link> components to avoid whole page refresh

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom";
 import { LogoutLink } from "./LogoutLink";
 
 export function Header() {
   return (
     <header>
       <nav>
-        <a href="/">Home</a> | <a href="/signup">Signup</a> | <a href="/login">Login</a> |{" "}
+        <Link to="/">Home</Link> | <Link to="/signup">Signup</Link> | <Link to="/login">Login</Link> |{" "}
         <a href="#products-index">All products</a> | <LogoutLink />
       </nav>
     </header>
