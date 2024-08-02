90585fabbf1b60bb62e6734451ce47ddb21efd80
Peter Jang <peter.x.jang@gmail.com>
2024-08-02 12:36:28 -0500

    Style products as cards in grid

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -2,14 +2,17 @@ export function ProductsIndex({ products, onShow }) {
   return (
     <div id="products-index">
       <h1 className="text-3xl font-bold underline">All products</h1>
-      {products.map((product) => (
-        <div key={product.id}>
-          <h2>{product.name}</h2>
-          <p>Price: {product.price}</p>
-          <p>{product.description}</p>
-          <button onClick={() => onShow(product)}>More info</button>
-        </div>
-      ))}
+      <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
+        {products.map((product) => (
+          <div key={product.id} className="rounded shadow-lg mb-4">
+            <h2 className="font-bold text-2xl">{product.name}</h2>
+            <img src="https://cdn.shopify.com/s/files/1/0070/7032/files/product-label-design.jpg?v=1680902906" alt="" />
+            <p>Price: {product.price}</p>
+            <p>{product.description}</p>
+            <button onClick={() => onShow(product)}>More info</button>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
