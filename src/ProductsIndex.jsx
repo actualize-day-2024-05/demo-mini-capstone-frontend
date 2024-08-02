850a319b01a7471ff772b4fb9edf4f247f06d467
Peter Jang <peter.x.jang@gmail.com>
2024-08-02 12:48:10 -0500

    Use images from json instead of placeholder

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -6,7 +6,7 @@ export function ProductsIndex({ products, onShow }) {
         {products.map((product) => (
           <div key={product.id} className="rounded shadow-lg mb-4">
             <h2 className="font-bold text-2xl">{product.name}</h2>
-            <img src="https://cdn.shopify.com/s/files/1/0070/7032/files/product-label-design.jpg?v=1680902906" alt="" />
+            <img src={product.primary_image_url} alt="" />
             <p>Price: {product.price}</p>
             <p>{product.description}</p>
             <button onClick={() => onShow(product)}>More info</button>
