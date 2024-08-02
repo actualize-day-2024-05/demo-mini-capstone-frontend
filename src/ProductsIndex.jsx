87f386109e549c6c2f70a040c1f0b536f83b69a4
Peter Jang <peter.x.jang@gmail.com>
2024-08-02 13:25:40 -0500

    Adjust card styling to align buttons to bottom of each card

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -4,15 +4,18 @@ export function ProductsIndex({ products, onShow }) {
       <h1 className="text-3xl font-bold underline mb-4">All products</h1>
       <div className="grid gap-4 grid-cols-1 md:grid-cols-3">
         {products.map((product) => (
-          <div key={product.id} className="rounded shadow-lg mb-4">
+          <div key={product.id} className="rounded shadow-lg mb-4 flex flex-col">
             <img src={product.primary_image_url} className="w-full aspect-[4/3] object-cover" alt="" />
-            <div className="p-4">
+            <div className="p-4 flex flex-col flex-grow">
               <h2 className="font-bold text-2xl mb-2">{product.name}</h2>
-              <div className="text-gray-700 text-base mb-2">
+              <div className="text-gray-700 text-base mb-2 flex-grow">
                 <p>Price: {product.price}</p>
                 <p>{product.description}</p>
               </div>
-              <button className="rounded border border-gray-300 p-2 hover:bg-gray-100" onClick={() => onShow(product)}>
+              <button
+                className="rounded border border-gray-300 p-2 hover:bg-gray-100 mt-auto"
+                onClick={() => onShow(product)}
+              >
                 More info
               </button>
             </div>
